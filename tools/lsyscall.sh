c909b8d24a410c4672f75e658e1eeb3f7bbcea68
Jianhong.Yin <yin-jianhong@163.com>
2020-01-20 15:05:54 +0800

    tools/lsyscall.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/lsyscall.sh
+++ tools/lsyscall.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-tableurl=https://raw.githubusercontent.com/hrw/syscalls-table/master/tables
-arch=$(arch)
-  is_available_url() { curl --connect-timeout 8 -m 16 --output /dev/null --silent --head --fail $1 &>/dev/null; }
-  is_intranet() { is_available_url http://download.devel.redhat.com; }
-  is_intranet && tableurl=http://download.devel.redhat.com/qa/rhts/lookaside/syscalls-table/tables
-
+Arch=$(arch)
 case $1 in
 alpha|arc|arm|arm64|armoabi|avr32|blackfin|c6x|cris|csky|frv|h8300|hexagon|i386|ia64|m32r|m68k|metag|microblaze|mips64|mips64n32|mipso32|mn10300|nds32|nios2|openrisc|parisc|powerpc|powerpc64|riscv32|riscv64|s390|s390x|score|sh|sh64|sparc|sparc64|tile|tile64|unicore32|x32|x86_64|xtensa)
-	arch=$1; shift;;
+	Arch=$1; shift;;
 esac
 
-tablefurl=$tableurl/syscalls-${arch}
-tables=$(curl -s -L $tablefurl)
+ausyscall() {
+	if [[ $# = 0 ]]; then
+		command ausyscall --dump | awk '{print $2, $1}'
+	else
+		local call=$1
+		local num=$call
+		local name=
+		read name num < <(command ausyscall $Arch $call 2>/dev/null)
+		if [[ -n "$name" ]]; then
+			echo $name $num
+		else
+			return 1
+		fi
+	fi
+}
 
-if [[ $# = 0 ]]; then
-	echo "$tables"
-else
-	for syscall; do
-		awk -v call=$syscall '$1 == call || $2 == call' <<<"$tables"
-	done
-fi
+lsyscall() {
+	local tableurl=https://raw.githubusercontent.com/hrw/syscalls-table/master/tables
+	is_available_url() { curl --connect-timeout 8 -m 16 --output /dev/null --silent --head --fail $1 &>/dev/null; }
+	is_intranet() { is_available_url http://download.devel.redhat.com; }
+	is_intranet && tableurl=http://download.devel.redhat.com/qa/rhts/lookaside/syscalls-table/tables
+
+	local tablefurl=$tableurl/syscalls-${Arch}
+	local tables=$(curl -s -L $tablefurl)
+
+	if [[ $# = 0 ]]; then
+		echo "$tables"
+	else
+		local call=$1
+		awk -v call=$call '$1 == call || $2 == call' <<<"$tables"
+	fi
+}
+
+ausyscall "$@" || lsyscall "$@"
