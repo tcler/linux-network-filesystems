7ccb97e9adaf24afe57d2f4e1553640aa2e1e0a0
Jianhong.Yin <yin-jianhong@163.com>
2020-01-31 15:59:35 +0800

    tools: add misc/rdma-vm.sh,rdma-peer.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ tools/misc/rdma-peer.sh
@@ -0,0 +1,3 @@
+yum install -y rdma opensm infiniband-diags librdmacm-utils
+systemctl start rdma
+systemctl start opensm
---
+++ tools/misc/rdma-vm.sh
@@ -0,0 +1,73 @@
+#RDMA Mellanox ConnectX-3 card SR-IOV configure:
+#ref: https://community.mellanox.com/s/article/howto-configure-sr-iov-for-connectx-3-with-kvm--infiniband-x  #configure sr-iov
+#ref: https://community.mellanox.com/s/article/howto-install-mlnx-ofed-driver  #install ofed driver
+#ref: https://cn.mellanox.com/products/infiniband-drivers/linux/mlnx_ofed  #ofed driver download page
+
+LANG=C
+
+# download driver and install dependency
+yum install lsof pciutils elfutils-libelf-devel kernel-rpm-macros python36-devel createrepo -y
+yum install tk gcc-gfortran tcsh -y
+wget ftp://fs-qe.usersys.redhat.com/pub/jiyin/MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64.tgz
+tar zxf MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64.tgz
+pushd MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64
+	# install MLNX_OFED driver
+	./mlnxofedinstall --add-kernel-support
+	systemctl stop opensm
+	lspci | grep Mellanox
+	/etc/init.d/openibd restart
+	lspci | grep Mellanox
+popd
+
+# enable SRIOV_EN and set NUM_OF_VFS
+/etc/init.d/opensmd restart
+mst start
+mst status
+#mlxconfig -d /dev/mst/mt4103_pciconf0 q
+#mlxconfig -d /dev/mst/mt4103_pciconf0 set SRIOV_EN=1 NUM_OF_VFS=4
+
+# reboot and confirm #seems no need
+#reboot
+#mst start; lspci | grep Mellanox; ibstat;
+
+# module configure, and reload
+systemctl stop opensm
+/etc/init.d/opensmd stop
+modprobe -r rpcrdma ib_srpt ib_isert
+echo "options mlx4_core port_type_array=1,1 num_vfs=4 probe_vf=2" >/etc/modprobe.d/mlx4_core.conf
+/etc/init.d/openibd restart
+lspci | grep Mellanox
+systemctl start opensm #need confirm
+ip a s
+
+which vm 2>/dev/null || {
+	which git &>/dev/null || yum install -y git
+	git clone https://github.com/tcler/kiss-vm-ns
+	make -C kiss-vm-ns
+}
+vm RHEL-8.1.1-updates-20200116.1 --nointeract -p "rdma opensm infiniband-diags librdmacm-utils"
+cat >hd.xml <<EOF
+<hostdev mode='subsystem' type='pci' managed='no'>
+<driver name='vfio'/>
+<source>
+    <address domain='0x0000' bus='0x04' slot='0x00' function='0x1'/>
+</source>
+</hostdev>
+EOF
+virsh nodedev-detach pci_0000_04_00_1
+virsh attach-device root-rhel-811-updates-202001161  hd.xml
+
+
+# guest:
+: <<EOF
+rdma opensm infiniband-diags librdmacm-utils
+echo "SVCRDMA_LOAD=yes" >>/etc/rdma/rdma.conf  #for nfs server
+modprobe svcrdma
+systemctl start rdma
+systemctl start opensm
+lspci
+ibstat
+ip link set dev ib1 up
+ip addr add 192.168.0.100/24 dev ib1
+ping 192.168.0.1  #bare-metal peer address
+EOF
