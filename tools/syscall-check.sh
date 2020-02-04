3a84bbcbbcc8212412c3992f180833d0dd8ddf0d
Jianhong.Yin <yin-jianhong@163.com>
2020-02-04 08:58:27 +0800

    tools: add syscall-check.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ tools/syscall-check.sh
@@ -0,0 +1,7 @@
+
+# check if syscall is implemented
+while read snum sname; do [[ $snum != [0-9]* || $sname = vhangup ]] && continue;  echo -e '#define _GNU_SOURCE\n#include <unistd.h>\n#include <sys/syscall.h>\nint main (void) { syscall ('$snum', 0, 0, 0, 0, 0, 0, 0, 0); }'|LANG=C gcc -x c -; LANG=C timeout 2 strace -e $sname ./a.out ;  done < <(ausyscall --dump)  |& grep ENOSYS
+
+# check if there is syscall wrapper in libc
+while read snum sname; do [[ $snum != [0-9]* ]] && continue;  echo -e '#define _GNU_SOURCE\n#include <unistd.h>\n#include <sys/syscall.h>\nint main (void) { syscall (SYS_'$sname', 0, 0, 0, 0, 0, 0, 0, 0); }'|LANG=C gcc -x c -;  done < <(ausyscall --dump) |& grep error:
+
