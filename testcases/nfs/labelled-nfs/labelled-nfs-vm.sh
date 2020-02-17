b8c72480ac462899bc8dbea6499a4990cdee006b
Jianhong.Yin <yin-jianhong@163.com>
2020-02-17 13:04:10 +0800

    testcases labelled-nfs-vm.sh: add Test 3 to cover export /usr/bin
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/labelled-nfs/labelled-nfs-vm.sh
+++ testcases/nfs/labelled-nfs/labelled-nfs-vm.sh
@@ -138,7 +138,7 @@ nfsusr=/mnt/nfsusr
 vmnfsservaddr=$(vm if $vmnfsserv)
 vm -v exec $vmnfsclnt -- mkdir -p $nfsmp $nfsusr
 vm -vx exec $vmnfsclnt -- mount $vmnfsservaddr:$nfsshare2 $nfsmp -overs=4.2,actimeo=1,sync
-vm -vx exec $vmnfsclnt -- mount $vmnfsservaddr:$nfsshareusr $nfsusr -overs=4.2,actimeo=1,sync,ro
+vm -vx exec $vmnfsclnt -- mount $vmnfsservaddr:$nfsshareusr $nfsusr -overs=4.2,actimeo=1,sync
 echo
 vm -vx exec $vmnfsclnt -- ls -lZ $nfsmp/testfile $nfsusr/bin/bash
 vm -vx exec $vmnfsserv -- ls -lZ $nfsshare2/testfile $nfsshareusr/bin/bash
@@ -153,5 +153,46 @@ vm -vx exec $vmnfsclnt -- "test '$scontextServ' = '$scontextClnt'"
 
 scontextServ2=$(vm -v exec $vmnfsserv -- stat -c %C $nfsshareusr/bin/bash)
 scontextClnt2=$(vm -v exec $vmnfsclnt -- stat -c %C $nfsusr/bin/bash)
+vm -vx exec $vmnfsclnt -- "test '$scontextServ2' = '$scontextClnt2'"
+
+echo
+vm -vx exec $vmnfsclnt -- umount $nfsmp
+vm -vx exec $vmnfsclnt -- umount $nfsusr
+
+
+#---------------------------------------------------------------
+# test3
+nfsshare3=/nfsshare3
+nfssharebin=/usr/bin
+echo -e "\n"
+echo "Test 3: export both $nfsshare3 and $nfssharebin" | GREP_COLORS='ms=44' grep --color=always .
+vm -v exec $vmnfsserv -- mkdir -p $nfsshare3
+vm -v exec $vmnfsserv -- "echo '$nfsshare3 *(rw,no_root_squash,security_label)' >/etc/exports"
+vm -v exec $vmnfsserv -- "echo '$nfssharebin *(ro,no_root_squash,security_label)' >>/etc/exports"
+vm -v exec $vmnfsserv -- systemctl restart nfs-server
+vm -v exec $vmnfsserv -- touch $nfsshare3/testfile
+vm -v exec $vmnfsserv -- ls -lZ $nfsshare3/testfile $nfssharebin/bash
+
+
+nfsmp=/mnt/nfsmp
+nfsbin=/mnt/nfsbin
+vmnfsservaddr=$(vm if $vmnfsserv)
+vm -v exec $vmnfsclnt -- mkdir -p $nfsmp $nfsbin
+vm -vx exec $vmnfsclnt -- mount $vmnfsservaddr:$nfsshare3 $nfsmp -overs=4.2,actimeo=1,sync
+vm -vx exec $vmnfsclnt -- mount $vmnfsservaddr:$nfssharebin $nfsbin -overs=4.2,actimeo=1,sync
+echo
+vm -vx exec $vmnfsclnt -- ls -lZ $nfsmp/testfile $nfsbin/bash
+vm -vx exec $vmnfsserv -- ls -lZ $nfsshare3/testfile $nfssharebin/bash
+echo
+vm -v exec $vmnfsclnt -- sleep 1
+vm -vx exec $vmnfsclnt -- ls -lZ $nfsmp/testfile $nfsbin/bash
+vm -vx exec $vmnfsserv -- ls -lZ $nfsshare3/testfile $nfssharebin/bash
+
+scontextServ=$(vm -v exec $vmnfsserv -- stat -c %C $nfsshare3/testfile)
+scontextClnt=$(vm -v exec $vmnfsclnt -- stat -c %C $nfsmp/testfile)
+vm -vx exec $vmnfsclnt -- "test '$scontextServ' = '$scontextClnt'"
+
+scontextServ2=$(vm -v exec $vmnfsserv -- stat -c %C $nfssharebin/bash)
+scontextClnt2=$(vm -v exec $vmnfsclnt -- stat -c %C $nfsbin/bash)
 vm -vx exec $vmnfsclnt -- "test '$scontextServ2' = '$scontextClnt2'"
 
