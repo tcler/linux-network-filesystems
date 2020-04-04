362fc41bca469de0e79d137d44a649b46f827a26
Jianhong.Yin <yin-jianhong@163.com>
2020-04-04 21:27:37 +0800

    testcase nfs multipath-ns: add 3rd connection test(nconnect=2)
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/multipath/multipath-ns.sh
+++ testcases/nfs/multipath/multipath-ns.sh
@@ -13,12 +13,15 @@ which ns &>/dev/null || {
 	sudo chmod +x /usr/bin/ns
 }
 
-ServerIP1=192.168.8.1
-ClientIP1=192.168.8.2
-ServerIP2=192.168.9.1
-ClientIP2=192.168.9.2
+ServerIP1=192.168.9.1
+ClientIP1=192.168.9.2
+ServerIP2=192.168.10.1
+ClientIP2=192.168.10.2
+ServerIP3=192.168.11.1
+ClientIP3=192.168.11.2
 ExportDir=/nfsshare
 MountPoint=/mnt/nfs
+MOUNT_OPTS="$*"
 
 ns 2>/dev/null
 
@@ -27,21 +30,23 @@ ns jj nsbase bash nfs-utils iproute iputils #firewalld
 
 systemctl stop firewalld
 
-ns -n serv --macvlan-ip $ServerIP1,$ServerIP2  --clone nsbase
+ns -n serv --macvlan-ip $ServerIP1,$ServerIP2,$ServerIP3  --clone nsbase
 ns exec -v serv -- systemctl stop firewalld
 ns exec -v serv -- mkdir -p $ExportDir
 ns exec -v serv -- touch $ExportDir/testfile
 ns exec -v serv -- "echo '$ExportDir *(rw,no_root_squash,security_label)' >/etc/exports"
 ns exec -v serv -- systemctl restart nfs-server
 
-ns -n c1 --macvlan-ip $ClientIP1,$ClientIP2 -bind=/usr -clone nsbase
+ns -n c1 --macvlan-ip $ClientIP1,$ClientIP2,$ClientIP3 -bind=/usr -clone nsbase
 ns exec -v   c1 -- systemctl stop firewalld
 ns exec -v   c1 -- mkdir -p $MountPoint
 
 ns exec -vx0 c1 -- showmount -e $ServerIP1
-ns exec -vx0 c1 -- mount -v $ServerIP1:/ $MountPoint -onconnect=16
+ns exec -vx0 c1 -- mount -v $ServerIP1:/ $MountPoint -onconnect=2 $MOUNT_OPTS
 ns exec -vx0 c1 -- showmount -e $ServerIP2
-ns exec -vx0 c1 -- mount -v $ServerIP2:/ $MountPoint -onconnect=16
+ns exec -vx0 c1 -- mount -v $ServerIP2:/ $MountPoint -onconnect=2 $MOUNT_OPTS
+ns exec -vx0 c1 -- showmount -e $ServerIP3
+ns exec -v   c1 -- mount -v $ServerIP3:/ $MountPoint -onconnect=2 $MOUNT_OPTS
 
 ns exec -v   c1 -- "mount -l | grep nfs4"
 ns exec -v   c1 -- mount -t nfs
