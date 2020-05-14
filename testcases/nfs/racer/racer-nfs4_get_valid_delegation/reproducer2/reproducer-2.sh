1c2fd162f8c70fb5a3aeb146e8d9b0319cd911a9
Jianhong Yin <jiyin@redhat.com>
2020-05-14 23:15:09 +0800

    testcases nfs racer: add comment info about kiss-vm
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/racer/racer-nfs4_get_valid_delegation/reproducer2/reproducer-2.sh
+++ testcases/nfs/racer/racer-nfs4_get_valid_delegation/reproducer2/reproducer-2.sh
@@ -11,6 +11,8 @@ mkdir -p /nfsshare
 echo "/nfsshare *(rw,no_root_squash)" | sudo tee /etc/exports
 sudo systemctl restart nfs-server
 
+# Here we assume that kiss-vm[1] is already installed in your system
+# [1] https://github.com/tcler/kiss-vm-ns
 vm $distro -n nfsclient2 cpus=2 -msize=4000 --kdump -f -p "gcc make kernel-devel elfutils-libelf-devel" --nointeract
 vm exec nfsclient2 -- mkdir /mnt/nfs
 scp -o Batchmode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r nfsv4-open-race root@nfsclient2:
