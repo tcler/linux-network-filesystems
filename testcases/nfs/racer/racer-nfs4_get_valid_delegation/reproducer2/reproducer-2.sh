4ab993b4601961716c04fbba77fcd87e617a2f88
Jianhong Yin <jiyin@redhat.com>
2020-05-14 16:28:34 +0800

    testcases nfs racer: fix permission and typo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/racer/racer-nfs4_get_valid_delegation/reproducer2/reproducer-2.sh
+++ testcases/nfs/racer/racer-nfs4_get_valid_delegation/reproducer2/reproducer-2.sh
@@ -8,10 +8,10 @@ distro=$1
 }
 
 mkdir -p /nfsshare 
-echo "/nfsshare *(rw,no_root_squash)" >/etc/exports
-systemctl restart nfs-server
+echo "/nfsshare *(rw,no_root_squash)" | sudo tee /etc/exports
+sudo systemctl restart nfs-server
 
-vm $distro -n nfsclient2 cpus=2 -msize=4000 --kdump -f -p "vim gcc make kernel-devel elfutils-libelf" --nointeract
+vm $distro -n nfsclient2 cpus=2 -msize=4000 --kdump -f -p "gcc make kernel-devel elfutils-libelf-devel" --nointeract
 vm exec nfsclient2 -- mkdir /mnt/nfs
 scp -o Batchmode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r nfsv4-open-race root@nfsclient2:
 
