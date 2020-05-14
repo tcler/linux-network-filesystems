41f3f56c430cafce76261b12f0d3ff5c2af220e2
Jianhong Yin <jiyin@redhat.com>
2020-05-14 17:05:55 +0800

    testcases nfs racer: fix typo
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/racer/racer-fscache/reproducer.sh
+++ testcases/nfs/racer/racer-fscache/reproducer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 server_distro=RHEL-7.8
-vm $server_distro -msize=4000 -f -n nfsserver -xdisk 16 --kdump --nointeract
 nfsserv=nfsserverX
+vm $server_distro -msize=4000 -f -n $nfsserv -xdisk 16 --kdump --nointeract
 vm exec $nfsserv -- mkdir /data
 vm exec $nfsserv -- mkfs.xfs /dev/vdb
 vm exec $nfsserv -- 'echo "/dev/vdb                /data      xfs    defaults        0 0" >>/etc/fstab'
@@ -12,8 +12,8 @@ vm exec $nfsserv -- systemctl restart nfs-server
 vm exec $nfsserv -- 'time for i in $(seq 1 4000); do dd if=/dev/zero of=/data/file$i.bin bs=1M count=4; done'
 
 client_distro=${1:-RHEL-7.7}
-vm $client_distro -cpus=8 -msize=8000 -f -n nfsclient -xdisk 4 -xdisk 4 --kdump -p "mdadm cachefilesd vim" --nointeract
 nfsclnt=nfsclientX
+vm $client_distro -cpus=8 -msize=7000 -f -n $nfsclnt -xdisk 4 -xdisk 4 --kdump -p "mdadm cachefilesd vim" --nointeract
 vm exec $nfsclnt -- 'mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/vd[bc]'
 vm exec $nfsclnt -- mkfs -t ext4 /dev/md0
 vm exec $nfsclnt -- 'echo "/dev/md0                /var/cache/fscache      ext4    defaults        0 0" >>/etc/fstab'
