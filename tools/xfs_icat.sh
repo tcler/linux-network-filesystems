3897980e20000362bf3c443e91332566e2d6519b
Jianhong Yin <jiyin@redhat.com>
2020-08-11 17:35:34 +0800

    tools/xfs_icat.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/xfs_icat.sh
+++ tools/xfs_icat.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #auth: yin-jianhong@163.com
 #just for learn xfs and funny
+#
+#ref: https://xfs.org/docs/xfsdocs-xml-dev/XFS_Filesystem_Structure/tmp/en-US/html/Data_Extents.html
+#ref: https://righteousit.wordpress.com/2018/05/23/xfs-part-2-inodes
 
 dev=$1
 inum=$2
@@ -29,7 +32,6 @@ inode_ver() {
 }
 
 #INFO=$(xfs_db -r $dev -c "inode $inum" -c "type inode" -c "print core.version")
-#read key eq g_iver <<<"$INFO"
 g_iver=$(inode_ver $dev $inum)
 test -n "$debug" && echo "core.version = $g_iver" >&2
 [[ "$g_iver" = 3 ]] && {
@@ -43,7 +45,7 @@ inode_extent_array() {
 	local _inum=$2
 
 	local agblocks=
-	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print agblocks')
+	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print agblocks' 2>/dev/null)
 	read key eq agblocks < <(grep agblocks <<<"$sbINFO")
 	local _agblocksB=$(echo "obase=2;$agblocks"|bc)
 	local agshift=${#_agblocksB}
@@ -53,7 +55,7 @@ inode_extent_array() {
 	_extentNum=$((16#$_extentNum))
 
 	echo "BMX[0-$((_extentNum-1))] = [startoff,startblock,blockcount,extentflag]"
-	local extentX= extent1B= flag= startoff= startblock= blockcount=
+	local extentX= extent1B= flag= startoff= startblockB= startblock= agnumLen= agnum= relativeblock= blockcount=
 	for ((i=0; i<_extentNum; i++)); do
 		extentX=$(dd status=none if=$_dev bs=1 skip=$((ioffsetD+dataForkOffset+i*16)) count=16 | hexdump -e '16/1 "%02x"')
 		extent1B=$(echo "ibase=16;obase=2;1${extentX^^}"|BC_LINE_LENGTH=256 bc)
@@ -99,28 +101,6 @@ ftypenum=${mode%????}
 ftypenum=$((8#$ftypenum))
 test -n "$debug" && echo "$INFO" >&2
 
-: <<\COMM
-inode_dump() {
-	local _dev=$1
-	local _inum=$2
-	IFS=' ()' read ioffsetX ioffsetD < <(xfs_db -r $_dev -c "convert inode $_inum fsbyte")
-	dd status=none if=$_dev bs=1 skip=$((ioffsetD)) count=256 | hexdump -e '16/1 "%02x " "\n"'
-}
-
-#work around for xfs_db bug
-inode_info() {
-	local _dev=$1
-	local _inum=$2
-	IFS=' ()' read ioffsetX ioffsetD < <(xfs_db -r $_dev -c "convert inode $_inum fsbyte")
-
-	local _mode=$(dd status=none if=$_dev bs=1 skip=$((ioffsetD+2)) count=2 | hexdump -e '1/1 "%02x"')
-	local _ftypenum=${_mode%???}
-	local _fsize=$(dd status=none if=$_dev bs=1 skip=$((ioffsetD+56)) count=8 | hexdump -e '8/1 "%02x"')
-	echo ftypenum = $((16#$_ftypenum))
-	echo fsize = $((16#$_fsize))
-}
-COMM
-
 ftype=${ftypes[$ftypenum]}
 [[ -z "$ftype" ]] && ftype="\033[41mnil\033[m"
 echo -e "core.format: $coreformat, ftype: $ftype($ftypenum), fsize: $fsize, iver: $g_iver, fsver: $fsver" >&2
@@ -131,7 +111,7 @@ extents_cat() {
 	local _fsize=$2
 	shift 2
 
-	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print blocksize')
+	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print blocksize' 2>/dev/null)
 	read key eq blocksize < <(grep blocksize <<<"$sbINFO")
 
 	local left=$_fsize
