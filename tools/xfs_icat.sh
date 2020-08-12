b553a6ffc4312f79df550bfe31a09d1afe850eed
Jianhong Yin <jiyin@redhat.com>
2020-08-12 15:41:33 +0800

    tools/xfs_icat.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/xfs_icat.sh
+++ tools/xfs_icat.sh
@@ -17,8 +17,8 @@ ftypes[6]=blkdev
 ftypes[8]=file
 ftypes[10]=symlink
 ftypes[12]=socket
-bmxField=u.bmx
-dataForkOffset=100
+bmxField=u3.bmx
+dataForkOffset=176
 
 inode_ver() {
 	local _dev=$1
@@ -35,9 +35,37 @@ inode_ver() {
 #INFO=$(xfs_db -r $dev -c "inode $inum" -c "type inode" -c "print core.version")
 g_iver=$(inode_ver $dev $inum)
 test -n "$debug" && echo "core.version = $g_iver" >&2
-[[ "$g_iver" = 3 ]] && {
-	bmxField=u3.bmx
-	dataForkOffset=176
+[[ "$g_iver" != 3 ]] && {
+	bmxField=u.bmx
+	dataForkOffset=100
+}
+
+_startblock2fsblock() {
+	local startblock=$1
+	local agshift=$2
+
+	[[ "${g_iver:-3}" != 3 ]] && {
+		echo -n "$startblock"
+		return
+	}
+
+	local startblockB=$(echo "obase=2; ibase=A; $startblock"|bc|xargs printf "%52s"|sed s/\ /0/)
+	local agnumLen=$((52-agshift))
+	local agnum=$(echo "ibase=2;obase=A;${startblockB:0:${agnumLen}}"|bc)
+	local relativeblock=$(echo "ibase=2;obase=A;${startblockB:${agnumLen}:${agshift}}"|bc)
+	echo -n $((agnum*agblocks+relativeblock))
+}
+startblock2fsblock() {
+	local startblock=$1
+	local dev=$2
+
+	local agblocks=
+	local sbINFO=$(xfs_db -r $dev -c "inode 0" -c "type sb" -c 'print agblocks' 2>/dev/null)
+	read key eq agblocks < <(grep agblocks <<<"$sbINFO")
+	local agblocksB=$(echo "obase=2;$agblocks"|bc)
+	local agshift=${#agblocksB}
+
+	_startblock2fsblock $startblocks $agshift
 }
 
 inode_extent_array() {
@@ -45,18 +73,12 @@ inode_extent_array() {
 	local _dev=$1
 	local _inum=$2
 
-	local agblocks=
-	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print agblocks' 2>/dev/null)
-	read key eq agblocks < <(grep agblocks <<<"$sbINFO")
-	local _agblocksB=$(echo "obase=2;$agblocks"|bc)
-	local agshift=${#_agblocksB}
-
 	IFS=' ()' read ioffsetX ioffsetD < <(xfs_db -r $_dev -c "convert inode $_inum fsbyte")
 	local _extentNum=$(dd status=none if=$_dev bs=1 skip=$((ioffsetD+76)) count=4 | hexdump -e '4/1 "%02x"')
 	_extentNum=$((16#$_extentNum))
 
 	echo "BMX[0-$((_extentNum-1))] = [startoff,startblock,blockcount,extentflag]"
-	local extentX= extent1B= flag= startoff= startblockB= startblock= agnumLen= agnum= relativeblock= blockcount=
+	local extentX= extent1B= flag= startoff= startblock= blockcount=
 	for ((i=0; i<_extentNum; i++)); do
 		extentX=$(dd status=none if=$_dev bs=1 skip=$((ioffsetD+dataForkOffset+i*16)) count=16 | hexdump -e '16/1 "%02x"')
 		extent1B=$(echo "ibase=16;obase=2;1${extentX^^}"|BC_LINE_LENGTH=256 bc)
@@ -65,25 +87,19 @@ inode_extent_array() {
 
 		startoff=$(echo "ibase=2;obase=A;${extent1B:2:54}"|bc)
 
-		startblockB=${extent1B:56:52}
-		startblock=$(echo "ibase=2;obase=A;${startblockB}"|bc)
+		startblock=$(echo "ibase=2;obase=A;${extent1B:56:52}"|bc)
 
 		blockcount=$(echo "ibase=2;obase=A;${extent1B:108:21}"|bc)
 
-		[[ "${g_iver:-3}" = 3 ]] && {
-			agnumLen=$((52-agshift))
-			agnum=$(echo "ibase=2;obase=A;${startblockB:0:${agnumLen}}"|bc)
-			relativeblock=$(echo "ibase=2;obase=A;${startblockB:${agnumLen}:${agshift}}"|bc)
-			echo " ${i}:[$startoff,$((agnum*agblocks+relativeblock)),$blockcount,$flag,$startblock]"
-
-			test -n "$debug" && echo "agshift: $agshift" >&2
-			test -n "$debug" && echo "agnumLen: $agnumLen" >&2
-			test -n "$debug" && echo "agnum: $agnum" >&2
-			test -n "$debug" && echo "relativeblock: $relativeblock" >&2
-		} || {
-			echo " ${i}:[$startoff,$startblock,$blockcount,$flag]"
-		}
+		echo " ${i}:[$startoff,$startblock,$blockcount,$flag]"
 	done
+}
+
+inode_extent_btree() {
+	local _dev=$1
+	local _inum=$2
+
+	#fixme:
 }
 
 [[ "${g_iver:-3}" = 3 ]] && {
@@ -112,13 +128,17 @@ extents_cat() {
 	local _fsize=$2
 	shift 2
 
-	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print blocksize' 2>/dev/null)
+	local sbINFO=$(xfs_db -r $_dev -c "inode 0" -c "type sb" -c 'print blocksize agblocks' 2>/dev/null)
 	read key eq blocksize < <(grep blocksize <<<"$sbINFO")
+	read key eq agblocks < <(grep agblocks <<<"$sbINFO")
+	local agblocksB=$(echo "obase=2;$agblocks"|bc)
+	local agshift=${#agblocksB}
 
 	local left=$_fsize
 	for extent; do
 		test -n "$debug" && echo "{extexts_cat} extent: $extent" >&2
 		read idx startoff startblock blockcount extentflag orig_startblock <<< "${extent//[:,\][]/ }"
+		startblock=$(_startblock2fsblock $startblock $agshift)
 		extentSize=$((blockcount * blocksize))
 		ddcount=$blockcount
 
