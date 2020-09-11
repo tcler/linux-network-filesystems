286c35c4655d19dd1bda4ac713e250c87eb18eb5
Jianhong Yin <jiyin@redhat.com>
2020-09-11 10:33:30 +0800

    testcases/nfs multipath-ns.sh: add xprt check
    the original test procedure was wrong, now nfs multipath is about
    mount option nconnect=N. add xprt check
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/multipath/multipath-ns.sh
+++ testcases/nfs/multipath/multipath-ns.sh
@@ -17,8 +17,6 @@ ServerIP1=192.168.9.1
 ClientIP1=192.168.9.2
 ServerIP2=192.168.10.1
 ClientIP2=192.168.10.2
-ServerIP3=192.168.11.1
-ClientIP3=192.168.11.2
 ExportDir=/nfsshare
 MountPoint=/mnt/nfs
 MOUNT_OPTS="$*"
@@ -30,34 +28,29 @@ ns jj nsbase bash nfs-utils iproute iputils #firewalld
 
 systemctl stop firewalld
 
-ns -n serv --macvlan-ip $ServerIP1,$ServerIP2,$ServerIP3  --clone nsbase
+ns -n serv --macvlan-ip $ServerIP1,$ServerIP2  --clone nsbase
 ns exec -v serv -- systemctl stop firewalld
 ns exec -v serv -- mkdir -p $ExportDir
 ns exec -v serv -- touch $ExportDir/testfile
 ns exec -v serv -- "echo '$ExportDir *(rw,no_root_squash,security_label)' >/etc/exports"
 ns exec -v serv -- systemctl restart nfs-server
 
-ns -n c1 --macvlan-ip $ClientIP1,$ClientIP2,$ClientIP3 -bind=/usr -clone nsbase
+ns -n c1 --macvlan-ip $ClientIP1,$ClientIP2 -bind=/usr -clone nsbase
 ns exec -v   c1 -- systemctl stop firewalld
 ns exec -v   c1 -- mkdir -p $MountPoint
 
 ns exec -vx0 c1 -- showmount -e $ServerIP1
-ns exec -vx0 c1 -- mount -v $ServerIP1:/ $MountPoint -onconnect=2 $MOUNT_OPTS
+ns exec -vx0 c1 -- mount -v -onconnect=2 $MOUNT_OPTS $ServerIP1:/ $MountPoint
 ns exec -vx0 c1 -- showmount -e $ServerIP2
-ns exec -vx0 c1 -- mount -v $ServerIP2:/ $MountPoint -onconnect=2 $MOUNT_OPTS
-ns exec -vx0 c1 -- showmount -e $ServerIP3
-ns exec -v   c1 -- mount -v $ServerIP3:/ $MountPoint -onconnect=2 $MOUNT_OPTS
+ns exec -v   c1 -- mount -v -onconnect=2 $MOUNT_OPTS $ServerIP2:/ $MountPoint
 
-ns exec -v   c1 -- "mount -l | grep nfs4"
-ns exec -v   c1 -- mount -t nfs
-ns exec -v   c1 -- mount -t nfs4
-
-#ns exec -v serv -- dd if=/dev/zero of=$ExportDir/testimage bs=1M count=1024
+ns exec -v   c1 -- mount -t nfs,nfs4
+ns exec -v   c1 -- grep xprt /proc/self/mountstats
+ns exec -v   c1 -- ss -nt "dst $ServerIP1"
 
 #please clean test env:
-ns exec -vx0 c1 -- umount $MountPoint
-ns exec -vx0 c1 -- umount $MountPoint
-ns exec -v   serv -- systemctl stop nfs-server
+ns exec -vx0 c1 -- umount -t nfs,nfs4 -a
+ns exec -v serv -- systemctl stop nfs-server
 
 ns delete c1
 ns delete serv
