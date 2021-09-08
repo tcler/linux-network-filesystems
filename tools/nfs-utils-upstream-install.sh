0402b3b45b2e40a4a2ac547a57f3552097290cc3
Jianhong Yin <jiyin@redhat.com>
2021-09-08 14:12:44 +0800

    misc: improve nfs-utils-upstream-install.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/nfs-utils-upstream-install.sh
+++ tools/nfs-utils-upstream-install.sh
@@ -3,11 +3,9 @@
 RC=1
 gitUrl=git://git.linux-nfs.org/projects/steved/nfs-utils.git
 
-yum install -y /usr/bin/aclocal /usr/bin/libtoolize rpcgen libtirpc-devel \
-    libevent-devel sqlite-devel device-mapper-devel libblkid-devel krb5-devel git
-
 if git clone $gitUrl; then
 	pushd nfs-utils
+		bash install-dep
 		if ./autogen.sh && ./configure && make && make install; then
 			mount.nfs -V
 			RC=0
