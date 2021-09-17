a596c6b098e5d0877173805efc796a85aed9c57a
Jianhong Yin <jiyin@redhat.com>
2021-09-17 12:00:18 +0800

    nfstest-install.sh: auto install git
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/nfstest-install.sh
+++ tools/nfstest-install.sh
@@ -2,6 +2,10 @@
 
 gitrepo=git://git.linux-nfs.org/projects/mora/nfstest.git
 
+[[ -f /usr/bin/git ]] || {
+	yum install -y git
+}
+
 echo "{nfstest-install} from $gitrepo"
 git clone $gitrepo || exit $?
 export PYTHONPATH=$PWD/nfstest
