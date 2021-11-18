4cb52fa8208e1adf2c9cd4ec65123b167e550e87
Jianhong Yin <jiyin@redhat.com>
2021-11-19 01:58:14 +0800

    rdma-host-sriov-passthrough.sh: update domain name
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/rdma-sriov/rdma-host-sriov-passthrough.sh
+++ tools/rdma-sriov/rdma-host-sriov-passthrough.sh
@@ -107,7 +107,7 @@ git clone https://github.com/tcler/make-windows-vm
 cd make-windows-vm
 ./make-win-vm.sh --image /var/lib/libvirt/images/Win2019-Evaluation.iso \
 	--os-variant win2k19 --vmname win2019-rdma \
-	--domain cifs-nfs.test -p ~Ocgxyz \
+	--domain win-rdma.test -p ~Ocgxyz \
 	--cpus 4 --ram 4096 --disk-size 60 --vncport 7799 \
 	--enable-kdc \
 	--hostif=ib6 \
