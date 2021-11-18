33b7928599b6c12f345cfba957a961af5a587d9d
Jianhong Yin <jiyin@redhat.com>
2021-11-18 12:58:57 +0800

    rdma-host-sriov-passthrough.sh: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- tools/rdma-sriov/rdma-host-sriov-passthrough.sh
+++ tools/rdma-sriov/rdma-host-sriov-passthrough.sh
@@ -12,42 +12,55 @@ grubby --args="intel_iommu=on iommu=pt" --update-kernel="$(/sbin/grubby --defaul
 reboot
 COMM
 
-# download driver and install dependency
-yum install lsof pciutils elfutils-libelf-devel kernel-rpm-macros python36-devel createrepo -y
-yum install tk gcc-gfortran tcsh -y
-wget ftp://fs-qe.usersys.redhat.com/pub/jiyin/MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64.tgz
-tar zxf MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64.tgz
-pushd MLNX_OFED_LINUX-4.7-3.2.9.0-rhel8.1-x86_64
-	# install MLNX_OFED driver
+# install dependency and download MLNX_OFED driver
+yum install -y lsof pciutils elfutils-libelf-devel \
+	kernel-rpm-macros python36-devel createrepo \
+	tk tcsh gcc-gfortran
+wget http://fs-qe.usersys.redhat.com/ftp/pub/jiyin/MLNX_OFED_LINUX-4.9-4.0.8.0-rhel8.4-x86_64.tgz
+
+# install MLNX_OFED driver
+tar zxf MLNX_OFED_LINUX-4.9-4.0.8.0-rhel8.4-x86_64.tgz
+pushd MLNX_OFED_LINUX-4.9-4.0.8.0-rhel8.4-x86_64
 	./mlnxofedinstall --add-kernel-support
+	lspci | grep Mellanox
+
 	systemctl stop opensm
-	lspci | grep Mellanox
+	/etc/init.d/opensmd stop
+	modprobe -r rpcrdma ib_srpt ib_isert
 	/etc/init.d/openibd restart
+
 	lspci | grep Mellanox
+	/etc/init.d/opensmd restart
 popd
 
-# enable SRIOV_EN and set NUM_OF_VFS
-/etc/init.d/opensmd restart
+# only once: update firmware enable SRIOV_EN and set NUM_OF_VFS
+: <<\COMM
 mst start
 mst status
-#mlxconfig -d /dev/mst/mt4103_pciconf0 q
-#mlxconfig -d /dev/mst/mt4103_pciconf0 set SRIOV_EN=1 NUM_OF_VFS=4
+
+mdevs=$(mst status | awk  '/^.dev.mst.*pciconf0/{print $1}')
+mdevs=$(for mdev in $mdevs; do
+	mlxconfig -d $mdev q |& awk '/Device:/{print $2}'
+done)
+for mdev in $mdevs; do
+	mlxconfig -d $mdev set SRIOV_EN=1 NUM_OF_VFS=16
+done
 
 # reboot and confirm #seems no need
-#reboot
-#mst start; lspci | grep Mellanox; ibstat;
+reboot
+COMM
 
 # module configure, and reload
 systemctl stop opensm
 /etc/init.d/opensmd stop
 modprobe -r rpcrdma ib_srpt ib_isert
-echo "options mlx4_core port_type_array=1,1 num_vfs=4 probe_vf=2" >/etc/modprobe.d/mlx4_core.conf
+echo "options mlx4_core port_type_array=1,1 num_vfs=8 probe_vf=0" >/etc/modprobe.d/mlx4_core.conf
 /etc/init.d/openibd restart
 lspci | grep Mellanox
 systemctl start opensm #need confirm
 ip a s
 
-# install RHEL-8.1 vm
+# install kiss-vm tool
 install-kiss-vm-ns() {
 	local _name=$1
 	local KissUrl=https://github.com/tcler/kiss-vm-ns
@@ -65,7 +78,11 @@ install-kiss-vm-ns() {
 }
 install-kiss-vm-ns vm
 
-vm RHEL-8.1.1-updates-20200116.1 --nointeract -p "rdma opensm infiniband-diags librdmacm-utils"
+# create RHEL-8.4 vm
+vm create RHEL-8.4.0 -n rhel-8-rdma --nointeract \
+	-p "rdma opensm infiniband-diags librdmacm-utils" \
+	--hostdev=pci_0000_04_00_1
+: <<\COMM
 cat >pci_0000_04_00_1.xml <<EOF
 <hostdev mode='subsystem' type='pci' managed='no'>
 <driver name='vfio'/>
@@ -75,12 +92,20 @@ cat >pci_0000_04_00_1.xml <<EOF
 </hostdev>
 EOF
 virsh nodedev-detach pci_0000_04_00_1
-virsh attach-device root-rhel-811-updates-202001161  pci_0000_04_00_1.xml
+virsh attach-device rhel-8-rdma pci_0000_04_00_1.xml
+COMM
 
-# install windows server vm
+# create windows server vm
 git clone https://github.com/tcler/make-windows-vm
 cd make-windows-vm
-./make-win-vm.sh --image /var/lib/libvirt/images/Win2019-Evaluation.iso     --os-variant win2k19 --vmname win2019-cifs-nfs --domain cifs-nfs.test -p ~Ocgxyz     --cpus 4 --ram 4096 --disk-size 60 --vncport 7799  ./answerfiles-cifs-nfs/* --enable-kdc
+./make-win-vm.sh --image /var/lib/libvirt/images/Win2019-Evaluation.iso \
+	--os-variant win2k19 --vmname win2019-cifs-nfs \
+	--domain cifs-nfs.test -p ~Ocgxyz \
+	--cpus 4 --ram 4096 --disk-size 60 --vncport 7799 \
+	--enable-kdc \
+	--hostdev=pci_0000_04_00_2 \
+	./answerfiles-cifs-nfs/*
+: <<\COMM
 cat >pci_0000_04_00_2.xml <<EOF
 <hostdev mode='subsystem' type='pci' managed='no'>
 <driver name='vfio'/>
@@ -91,6 +116,7 @@ cat >pci_0000_04_00_2.xml <<EOF
 EOF
 virsh nodedev-detach pci_0000_04_00_2
 virsh attach-device win2019-cifs-nfs  pci_0000_04_00_2.xml
+COMM
 # download windows driver
 # ref: https://cn.mellanox.com/products/adapter-software/ethernet/windows/winof-2
 # ref: http://www.mellanox.com/downloads/WinOF/MLNX_VPI_WinOF-5_50_52000_All_win2019_x64.exe
@@ -98,14 +124,13 @@ virsh attach-device win2019-cifs-nfs  pci_0000_04_00_2.xml
 
 # guest:
 : <<EOF
-rdma opensm infiniband-diags librdmacm-utils
-echo "SVCRDMA_LOAD=yes" >>/etc/rdma/rdma.conf  #for nfs server
-modprobe svcrdma
-systemctl start rdma
+yum install -y rdma opensm infiniband-diags librdmacm-utils
+sed -i -e '/rdma/s/^#//' -e 's/rdma=n/rdma=y/' /etc/nfs.conf
+systemctl start nfs-server
 systemctl start opensm
 lspci
 ibstat
 ip link set dev ib1 up
-ip addr add 192.168.0.100/24 dev ib1
-ping 192.168.0.1  #bare-metal peer address
+ip addr add 192.168.1.100/24 dev ib1
+ping 192.168.1.1  #bare-metal peer address
 EOF
