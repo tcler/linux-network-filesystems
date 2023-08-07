d77fda2b952d7ea4db9b95d5725bc16ce6800c81
Jianhong Yin <jiyin@redhat.com>
2023-08-08 00:08:16 +0800

    testcases/nfs/nfstest: fix ssh-copy-id code
    still fail on RHEL-9, need to update kiss-vm utils
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/nfstest/nfstest_cache/main.sh
+++ testcases/nfs/nfstest/nfstest_cache/main.sh
@@ -11,21 +11,22 @@ nfsmp=/mnt/nfsmp
 #create nfs-server vm
 [[ $1 != -* ]] && { distro="$1"; shift; }
 distro=${distro:-9}
+passwd=redhat
 vmserv=nfs-server
 vmclntx=nfs-clientx
 vmclnt=nfs-client
-vm create $distro -n $vmserv -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmserv -m 4G -f -nointeract -p 'nfs-utils wireshark' --sa
 vm -v cpto $vmserv /usr/bin/make-nfs-server.sh .
 vm -v exec $vmserv -- bash make-nfs-server.sh
 vm -v exec $vmserv -- mkdir -p /nfsshare/rw/testdir
 vm -v exec $vmserv -- touch /nfsshare/rw/testdir/file{1..128}
 servaddr=$(vm ifaddr $vmserv)
 
-vm create $distro -n $vmclntx -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmclntx -m 4G -f -nointeract -p 'nfs-utils wireshark' --sa
 vm exec -v $vmclntx -- showmount -e $servaddr
 clntxaddr=$(vm ifaddr $vmclntx)
 
-vm create $distro -n $vmclnt -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmclnt -m 4G -f -nointeract -p 'nfs-utils wireshark expect' --sa
 vm exec -v $vmclnt -- showmount -e $servaddr
 clntaddr=$(vm ifaddr $vmclnt)
 
@@ -35,6 +36,9 @@ NIC=eth0
 vm -v cpto $vmclnt /usr/bin/install-nfstest.sh .
 vm -v exec $vmclnt -- bash install-nfstest.sh
 vm -v exec $vmclnt -- bash -c 'cat /tmp/nfstest.env >>/etc/bashrc'
+vm -v exec foo@$vmclnt -- "ssh-keygen -q -t ecdsa -f ~/.ssh/id_ecdsa -N ''"
+vm -v exec foo@$vmclnt -- expect -c "spawn ssh-copy-id -oStrictHostKeyChecking=no -f $clntxaddr; expect {*assword:} {send \"$passwd\\n\"}; expect eof"
+vm -v exec foo@$vmclnt -- expect -c "spawn ssh-copy-id -oStrictHostKeyChecking=no -f root@$clntxaddr; expect {*assword:} {send \"$passwd\\n\"}; expect eof"
+
 vm -v exec $vmclnt -- ip link set "$NIC" promisc on
-vm -v exec foo@$vmclnt -- ssh-copy-id -oStrictHostKeyChecking=no -f $clntxaddr
 vm -v exec foo@$vmclnt -- nfstest_cache --server $servaddr --client $clntxaddr --export=$expdir --mtpoint=$nfsmp --interface=$NIC --nfsversion=4.2 "$@"
--- testcases/nfs/nfstest/nfstest_delegation/main.sh
+++ testcases/nfs/nfstest/nfstest_delegation/main.sh
@@ -11,21 +11,22 @@ nfsmp=/mnt/nfsmp
 #create nfs-server vm
 [[ $1 != -* ]] && { distro="$1"; shift; }
 distro=${distro:-9}
+passwd=redhat
 vmserv=nfs-server
 vmclntx=nfs-clientx
 vmclnt=nfs-client
-vm create $distro -n $vmserv -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmserv -m 4G -f -nointeract -p 'nfs-utils wireshark' --sa
 vm -v cpto $vmserv /usr/bin/make-nfs-server.sh .
 vm -v exec $vmserv -- bash make-nfs-server.sh
 vm -v exec $vmserv -- mkdir -p /nfsshare/rw/testdir
 vm -v exec $vmserv -- touch /nfsshare/rw/testdir/file{1..128}
 servaddr=$(vm ifaddr $vmserv)
 
-vm create $distro -n $vmclntx -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmclntx -m 4G -f -nointeract -p 'nfs-utils wireshark' --sa
 vm exec -v $vmclntx -- showmount -e $servaddr
 clntxaddr=$(vm ifaddr $vmclntx)
 
-vm create $distro -n $vmclnt -m 4G -f -nointeract -p 'nfs-utils wireshark tmux' --sa
+vm create $distro -n $vmclnt -m 4G -f -nointeract -p 'nfs-utils wireshark expect iproute-tc kernel-modules-extra' --sa
 vm exec -v $vmclnt -- showmount -e $servaddr
 clntaddr=$(vm ifaddr $vmclnt)
 
@@ -35,6 +36,12 @@ NIC=eth0
 vm -v cpto $vmclnt /usr/bin/install-nfstest.sh .
 vm -v exec $vmclnt -- bash install-nfstest.sh
 vm -v exec $vmclnt -- bash -c 'cat /tmp/nfstest.env >>/etc/bashrc'
+vm -v exec $vmclnt -- "ssh-keygen -q -t ecdsa -f ~/.ssh/id_ecdsa -N ''"
+vm -v exec $vmclnt -- expect -c "spawn ssh-copy-id -oStrictHostKeyChecking=no -f $clntxaddr; expect {*assword:} {send \"$passwd\\n\"}; expect eof"
+
 vm -v exec $vmclnt -- ip link set "$NIC" promisc on
-vm -v exec $vmclnt -- ssh-copy-id -oStrictHostKeyChecking=no -f $clntxaddr
 vm -v exec $vmclnt -- nfstest_delegation --server $servaddr --client $clntxaddr --client-nfsvers=4.0,4.1,4.2 --export=$expdir --nfsversion=4.2 "$@"
+
+#2174870#c5
+vm -v exec $vmclnt -- tc qdisc add dev $NIC root netem delay 14ms
+vm -v exec $vmclnt -- nfstest_delegation --server=$servaddr --client $clntxaddr --export=$expdir --nfsversion=4.2 --sec=krb5 --nconnect 16
