8a6c322857d53ac15d14e2d5f3892fafb5628a85
Jianhong Yin <jiyin@redhat.com>
2023-08-15 19:58:47 +0800

    testcases/nfs nfstest_cache: optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/nfstest/nfstest_cache/main.sh
+++ testcases/nfs/nfstest/nfstest_cache/main.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 #
-
 . /usr/lib/bash/libtest || { echo "{ERROR} 'kiss-vm-ns' is required, please install it first" >&2; exit 2; }
 
-#export share dir by nfs
 _USER=$(whoami)
 [[ $(id -u) = 0 && -n "$SUDO_USER" ]] && _USER=$SUDO_USER
 nfsmp=/mnt/nfsmp
@@ -11,7 +9,6 @@ nfsmp=/mnt/nfsmp
 #create nfs-server vm
 [[ $1 != -* ]] && { distro="$1"; shift; }
 distro=${distro:-9}
-passwd=redhat
 vmserv=nfs-server
 vmclntx=nfs-clientx
 vmclnt=nfs-client
@@ -23,22 +20,19 @@ vm -v exec $vmserv -- touch /nfsshare/rw/testdir/file{1..128}
 servaddr=$(vm ifaddr $vmserv)
 
 vm create $distro -n $vmclntx -m 4G -f -nointeract -p 'vim nfs-utils wireshark python3' --sa
-vm exec -v $vmclntx -- showmount -e $servaddr
-clntxaddr=$(vm ifaddr $vmclntx)
 
 vm create $distro -n $vmclnt -m 4G -f -nointeract -p 'vim nfs-utils wireshark expect iproute-tc kernel-modules-extra' --sa
 vm exec -v $vmclnt -- showmount -e $servaddr
-clntaddr=$(vm ifaddr $vmclnt)
 
 #nfstest_cache
 expdir=/nfsshare/rw
 NIC=eth0
-vm -v cpto $vmclnt /usr/bin/install-nfstest.sh .
-vm -v exec $vmclnt -- bash install-nfstest.sh
+clntxaddr=$(vm ifaddr $vmclntx)
+vm -v cpto $vmclnt /usr/bin/install-nfstest.sh /usr/bin/ssh-copy-id.sh /usr/bin/.
+vm -v exec $vmclnt -- install-nfstest.sh
 vm -v exec $vmclnt -- bash -c 'cat /tmp/nfstest.env >>/etc/bashrc'
-vm -v exec foo@$vmclnt -- "ssh-keygen -q -t ecdsa -f ~/.ssh/id_ecdsa -N ''"
-vm -v exec foo@$vmclnt -- expect -c "spawn ssh-copy-id -oStrictHostKeyChecking=no -f $clntxaddr; expect {*assword:} {send \"$passwd\\n\"}; expect eof"
-vm -v exec foo@$vmclnt -- expect -c "spawn ssh-copy-id -oStrictHostKeyChecking=no -f root@$clntxaddr; expect {*assword:} {send \"$passwd\\n\"}; expect eof"
+vm -v exec foo@$vmclnt -- ssh-copy-id.sh $clntxaddr foo redhat
+vm -v exec foo@$vmclnt -- ssh-copy-id.sh $clntxaddr root redhat
 
 vm -v exec $vmclnt -- ip link set "$NIC" promisc on
 vm -v exec foo@$vmclnt -- nfstest_cache --server $servaddr --client $clntxaddr --export=$expdir --mtpoint=$nfsmp --interface=$NIC --nfsversion=4.2 "$@"
