c638205482f09745cfa1760a7e4d16d94357ae19
Jianhong Yin <jiyin@redhat.com>
2023-08-18 13:54:22 +0800

    testcases/nfs nfstest: add new test case nfstest_interop
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ testcases/nfs/nfstest/nfstest_interop/main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+. /usr/lib/bash/libtest || { echo "{ERROR} 'kiss-vm-ns' is required, please install it first" >&2; exit 2; }
+
+distro=${1:-9}
+clientvm=${2:-rhel-client}
+
+### __prepare__ test env build
+#create Windows AD server, ONTAP simulator and client VMs
+make-ontap-with-windows-ad.sh $distro $clientvm
+
+#install nfstest on $clientvm
+vm cpto $clientvm /usr/bin/install-nfstest.sh /usr/bin/.
+vm exec -vx $clientvm -- install-nfstest.sh
+vm exec -vx $clientvm -- bash -c 'cat /tmp/nfstest.env >>/etc/bashrc'
+
+ONTAP_ENV_FILE=/tmp/ontap2info.env
+source "$ONTAP_ENV_FILE"
+vm exec -v  $clientvm -- nfstest_interop --server ${NETAPP_NAS_HOSTNAME} --export=${NETAPP_NFS_SHARE} --sec=krb5 --datadir datadir --nfsversion=4.2
