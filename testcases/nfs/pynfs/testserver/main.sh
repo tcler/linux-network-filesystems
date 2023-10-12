9d4f36c02a6124d9ee72f68962c3ff43d0df8b4c
Jianhong Yin <jiyin@redhat.com>
2023-10-12 18:38:29 +0800

    testcases/nfs: add new test pynfs/testserver
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ testcases/nfs/pynfs/testserver/main.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+. /usr/lib/bash/libtest || { echo "{ERROR} 'kiss-vm-ns' is required, please install it first" >&2; exit 2; }
+
+#create nfs-server vm
+[[ $1 != -* ]] && { distro="$1"; shift; }
+distro=${distro:-9}
+nfsserv=nfs-server
+nfsclnt=nfs-client
+
+#download image file
+stdlog=$(trun vm create $distro --downloadonly |& tee /dev/tty)
+imgf=$(sed -n '${s/^.* //;p}' <<<"$stdlog")
+
+trun -tmux vm create $distro -n $nfsserv -m 4G -f -nointeract -p vim,nfs-utils,tmux,wireshark -I=$imgf "$@"
+trun       vm create $distro -n $nfsclnt -m 4G -f -nointeract -p vim,nfs-utils,tmux,wireshark -I=$imgf "$@"
+echo "{INFO} waiting all vm create process finished ..."
+while ps axf|grep tmux.new.*-d.vm.creat[e]; do sleep 10; done
+
+vm cpto -v $nfsserv /usr/bin/make-nfs-server.sh .
+vm exec -v $nfsserv -- bash make-nfs-server.sh
+vm exec -v $nfsserv -- mkdir -p /nfsshare/rw/testdir
+vm exec -v $nfsserv -- touch /nfsshare/rw/testdir/file{1..128}
+servaddr=$(vm ifaddr $nfsserv)
+
+vm exec -v $nfsclnt -- showmount -e $servaddr
+
+#nfstest_alloc
+expdir=/nfsshare/rw
+nfsmp=/mnt/nfsmp
+NIC=eth0
+vm cpto -v $nfsclnt /usr/bin/install-pynfs.sh /usr/bin/
+vm exec -v $nfsclnt -- install-pynfs.sh
+vm exec -v $nfsclnt -- ip link set "$NIC" promisc on
+
+distro=$(vm homedir $nfsclnt|awk -F/ 'NR==1{print $(NF-1)}')
+resdir=~/testres/$distro/pynfs
+mkdir -p $resdir
+vm exec -v $nfsclnt -- testserver.py --maketree $servaddr:$expdir all |& tee $resdir/testserver.log
