11c86d2c0cf2f7a3bb9a00206489bd172f24b27e
Jianhong Yin <jiyin@redhat.com>
2024-03-08 19:44:05 +0800

    testcases/xfs/xfstests: add env MOUNT_OPTION and optimize code
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/xfs/xfstests/main.sh
+++ testcases/xfs/xfstests/main.sh
@@ -10,20 +10,21 @@
 #NOURING=yes
 #FSTYPE=xfs
 #MKFS_OPTS=
+#MOUNT_OPTIONS=
 
 [[ $1 != -* ]] && { distro="$1"; shift 1; }; at=("$@")
 distro=${distro:-9}
 vmname=fstest; for ((i=0;i<${#at};i++)); do [[ ${at[$i]} = -n ]] && vmname=${at[$((i+1))]}; done
 
 ### __prepare__ test env build
-stdlog=$(trun vm create $distro --downloadonly |& tee /dev/tty)
+stdlog=$(trun vm create $distro --downloadonly "$@" |& tee /dev/tty)
 imgf=$(sed -n '${s/^.* //;p}' <<<"$stdlog")
 
 fs=${FSTYPE:-xfs}
 mkfsOpt="${MKFS_OPTS} "
 case $fs in ext*) mkfsOpt+=-F;; btrfs|xfs) mkfsOpt+=-f;; esac
 trun vm create -n $vmname $distro --msize 4096 -p git,tmux,vim --nointeract -I=$imgf -f \
-	--xdisk=16,${fs},bus=sata --xdisk=16,${fs},bus=sata --xdisk=16,${fs},bus=sata "$@"
+	--xdisk=16,${fs} --xdisk=16,${fs} --xdisk=16,${fs} "$@"
 
 vm cpto -v  $vmname /usr/bin/xfstests-install.sh /usr/bin/make-nfs-server.sh /usr/bin/.
 vm exec -vx $vmname -- "xfstests-install.sh $NOURING" || exit 1
@@ -32,14 +33,17 @@ vm exec -vx $vmname -- "xfstests-install.sh $NOURING" || exit 1
 #prepare TEST_DEV TEST_DIR SCRATCH_DEV SCRATCH_MNT for xfstests
 vm exec -v $vmname -- "mkdir -p /mnt/xfstests_test /mnt/xfstests_scratch"
 vm exec -v $vmname -- "useradd -m fsgqa; useradd 123456-fsgqa; useradd fsgqa2; groupadd fsgqa"
+
+devs=($(vm exec -v $vmname -- lsblk -nio NAME,LABEL | awk '$2 ~ /.*xdisk[0-9]/{print substr($1,3)}'))
 vm exec -v $vmname -- "cat >/var/lib/xfstests/local.config <<EOF
-export TEST_DEV=/dev/sda1
+export TEST_DEV=/dev/${devs[0]}
 export TEST_DIR=/mnt/xfstests_test
-export SCRATCH_DEV=/dev/sdb1
+export SCRATCH_DEV=/dev/${devs[1]}
 export SCRATCH_MNT=/mnt/xfstests_scratch
-export LOGWRITES_DEV=/dev/sdc1
+export LOGWRITES_DEV=/dev/${devs[2]}
+export MOUNT_OPTIONS='${MOUNT_OPTION:--o dax}'
 EOF"
-[[ -n "$MKFS_OPTS" ]] && vm exec -vx $vmname -- 'for dev in /dev/sd{a..c}1; do '"mkfs.${fs} $mkfsOpt"' $dev; done'
+[[ -n "$MKFS_OPTS" ]] && vm exec -vx $vmname -- "for dev in ${devs[@]}; do mkfs.${fs} $mkfsOpt /dev/\$dev; done"
 
 distro=$(vm homedir $nfsclnt|awk -F/ 'NR==1{print $(NF-1)}')
 resdir=~/testres/$distro/nfstest
