9d5c1e1a36c6aab43fadb966684d86e5772159e7
Jianhong Yin <jiyin@redhat.com>
2024-03-20 17:38:45 +0800

    testcases/nfs nfstest_pnfs: fix connection fail
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/nfstest/nfstest_pnfs/main.sh
+++ testcases/nfs/nfstest/nfstest_pnfs/main.sh
@@ -11,17 +11,17 @@ nfsmp=/mnt/nfsmp
 stdlog=$(trun vm create $distro --downloadonly "$@" |& tee /dev/tty)
 imgf=$(sed -n '${s/^.* //;p}' <<<"$stdlog")
 
-#create vm as nfs client
-trun -tmux vm create $distro -n $nfsclnt -m 4G -f -nointeract --net ontap2-data -p nfs-utils,expect,iproute-tc,kernel-modules-extra -I=$imgf "$@"
-
 #create netapp ontap-simulator
 trun -x0 make-ontap-simulator.sh $distro $nfsclnt || exit $?
+ONTAP_ENV_FILE=/tmp/ontap2info.env
+source "$ONTAP_ENV_FILE"
 
 echo "{INFO} waiting all vm create process finished ..."
 while ps axf|grep tmux.new.*$$-$USER.*-d.vm.creat[e]; do sleep 16; done
 
-servaddr=192.168.20.21
-vm exec -v $nfsclnt -- showmount -e $servaddr
+oservaddr=$NETAPP_NAS_IP
+lservaddr=192.168.20.21
+vm exec -v $nfsclnt -- showmount -e $lservaddr
 vm exec -v $nfsclnt -- mkdir -p $nfsmp
 
 #nfstest_pnfs
@@ -38,7 +38,7 @@ resdir=~/testres/$distrodir/nfstest
 mkdir -p $resdir
 {
   vm exec -v $nfsclnt -- uname -r;
-  vm exec -v $nfsclnt -- nfstest_pnfs --server $servaddr --export=$expdir --mtpoint=$nfsmp --interface=$NIC --nfsversion=4.2;
+  vm exec -v $nfsclnt -- nfstest_pnfs --server $oservaddr --export=$expdir --mtpoint=$nfsmp --interface=$NIC --nfsversion=4.2;
 } |& tee $resdir/pnfs.log
 
 vm stop $nfsclnt
