7e65b2926548643f178faa7fc4e101a3a7871542
Jianhong Yin <jiyin@redhat.com>
2024-06-12 08:51:02 +0800

    nfs/multipath-and-nconnect: fix and optimize main-vm.sh
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/multipath-and-nconnect/main-vm.sh
+++ testcases/nfs/multipath-and-nconnect/main-vm.sh
@@ -20,6 +20,7 @@ imgf=$(sed -rn '${/^-[-rwx]{9}.? /{s/^.* //;p}}' <<<"$stdlog")
 
 S=nfs-nconn-serv
 C=nfs-nconn-clnt
+pkgs=nfs-utils,firewalld
 
 [[ -z "$distro" ]] && {
 	echo "Usage: $0 <distro> [mount options]"
@@ -29,38 +30,40 @@ C=nfs-nconn-clnt
 vm netcreate netname=$netname1 brname=$brname1 subnet=$subnet1
 vm netinfo $netname1
 
-vm create "$distro" -n $S -p "nfs-utils" --nointeract --net default --net $netname1 -I=$imgf -f "$@"
-vm create "$distro" -n $C -p "nfs-utils" --nointeract --net default --net $netname1 -I=$imgf -f "$@"
+vm create "$distro" -n $S -p $pkgs --nointeract --net default --net $netname1 -I=$imgf -f "$@"
+vm create "$distro" -n $C -p $pkgs --nointeract --net default --net $netname1 -I=$imgf -f "$@"
 timeout 300 vm port-available -w $S || { echo "{TENV:ERROR} vm port 22 not available" >&2; exit 124; }
 
-servIpAddrs=$(vmrunx - $S -- ip a s)
+servIpAddrs=$(vm exec $S -- ip a s)
 echo "$servIpAddrs"
 
-Saddr0=$(echo "$servIpAddrs" | awk -F'[/ ]+' '/inet 192.168.122.[0-9]+/{print $3}')
-Saddr1=$(echo "$servIpAddrs" | awk -F'[/ ]+' "/inet 192.168.$subnet1.[0-9]+/{print \$3}")
+defaultNetIp=$(vm netinfo default | awk -F"[ =']+" '/ip address=/{print $4}')
+Saddr0=$(echo "$servIpAddrs" | awk -F'[/ ]+' "/inet ${defaultNetIp%.*}.[0-9]+/{print \$3}")
+Saddr1=$(echo "$servIpAddrs" | awk -F'[/ ]+' "/inet 192.168.${subnet1}.[0-9]+/{print \$3}")
 
-vmrunx -   $S -- mkdir -p $ExportDir
-vmrunx -   $S -- touch $ExportDir/testfile
-vmrunx -   $S -- "echo '$ExportDir *(rw,no_root_squash,insecure,security_label)' >/etc/exports"
-vmrunx -   $S -- systemctl restart nfs-server
-vmrunx -   $S -- firewall-cmd --permanent --add-service={mountd,nfs,rpc-bind}
-vmrunx -   $S -- firewall-cmd --reload
+vmrunx - $S  mkdir -p $ExportDir
+vmrunx - $S  touch $ExportDir/testfile
+vmrunx - $S  "echo '$ExportDir *(rw,no_root_squash,insecure,security_label)' >/etc/exports"
+vmrunx - $S  systemctl restart nfs-server
+vmrunx - $S  firewall-cmd --permanent --add-service={mountd,nfs,rpc-bind}
+vmrunx - $S  firewall-cmd --reload
 
-vmrunx -   $C -- mkdir -p $MountPoint
-vmrunx 0 $C -- showmount -e $Saddr0
-vmrunx 0 $C -- mount $MOUNT_OPTS $Saddr0:$ExportDir $MountPoint
-vmrunx 0 $C -- showmount -e $Saddr1
-vmrunx -   $C -- mount $MOUNT_OPTS $Saddr1:$ExportDir $MountPoint
+vmrunx - $C  mkdir -p $MountPoint
+vmrunx 0 $C  showmount -e $Saddr0
+vmrunx 0 $C  mount $MOUNT_OPTS $Saddr0:$ExportDir $MountPoint
+vmrunx 0 $C  showmount -e $Saddr1
+vmrunx - $C  mount $MOUNT_OPTS $Saddr1:$ExportDir $MountPoint
 
-vmrunx -   $C -- mount -t nfs,nfs4
-vmrunx -   $C -- grep xprt /proc/self/mountstats
-vmrunx -   $C -- ss -nt "dst $Saddr0"
-vmrunx -   $C -- ss -nt "dst $Saddr1"
+vmrunx - $C  mount -t nfs,nfs4
+vmrunx - $C  grep xprt /proc/self/mountstats
+vmrunx - $C  ss -nt "dst $Saddr0"
+vmrunx - $C  ss -nt "dst $Saddr1"
 
-vmrunx -   $C -- mount -t nfs,nfs4
-vmrunx 0 $C -- umount -t nfs,nfs4 -a
+vmrunx - $C  mount -t nfs,nfs4
+vmrunx 0 $C  umount -t nfs,nfs4 -a
 
 #please clean test env:
-vm del $C
-vm del $S
+vm del $C $S
 vm netdel $netname1
+
+tcnt
