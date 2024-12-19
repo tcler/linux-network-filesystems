7b58eaf42be45ae37560a74419748859dac333ef
Jianhong Yin <jiyin@redhat.com>
2024-12-19 15:14:30 +0800

    testcases nfstest_pnfs: add base pnfs test before nfstest_pnfs
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/nfstest/nfstest_pnfs/main-ontap.sh
+++ testcases/nfs/nfstest/nfstest_pnfs/main-ontap.sh
@@ -42,6 +42,15 @@ mkdir -p $resdir
 {
   vmrunx - $nfsclnt -- uname -r;
   trun -tmux=$_test-client.console -logpath=$resdir vm console $nfsclnt
+  vmrunx 0 $nfsclnt -- mount -overs=4.1,noresvport $NETAPP_NAS_IP_LOC:$NETAPP_NFS_SHARE2 $nfsmp
+  vmrunx 0 $nfsclnt -- tmux new -s ddSession -d "dd if=/dev/zero of=$nfsmp/testfile bs=1b count=1000000"
+  vmrunx 0 $nfsclnt -- 'sleep 1; ss -nat | grep :2049'
+  vmrunx 0 $nfsclnt -- 'ss -nat | grep 192.168.20.21:2049'
+  vmrunx 0 $nfsclnt -- 'ss -nat | grep 192.168.20.22:2049'
+  vmrunx 0 $nfsclnt -- tmux kill-session -t ddSession
+  vmrunx 0 $nfsclnt -- rm -f $nfsmp/testfile
+  vmrunx 0 $nfsclnt -- umount $nfsmp
+
   vmrunx - $nfsclnt -- nfstest_pnfs --server $lservaddr --export=$expdir --mtpoint=$nfsmp --interface=$NIC --trcdelay=3 --client-ipaddr=$clntaddr --nfsversion=4.2 $TESTS;
   trun -x1-255 grep RI[P]: $resdir/*console.log
 } |& tee $resdir/std.log
