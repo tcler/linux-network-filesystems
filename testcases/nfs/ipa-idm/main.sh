e2b37e27dbf660cf1898609a4709fb46be9a8406
Jianhong Yin <jiyin@redhat.com>
2025-08-30 12:14:20 +0800

    nfs/ipa-idm: fix export path
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/ipa-idm/main.sh
+++ testcases/nfs/ipa-idm/main.sh
@@ -15,6 +15,7 @@ nfsserv=ipa-nfs-server
 nfsclnt=ipa-nfs-client
 password=redhat123
 NFSSHARE=/nfsshare
+NFSROOT=/var
 
 stopvms() { [[ "${KEEPVM:-${KEEPVMS}}" != yes ]] && vm stop $ipaserv $nfsserv $nfsclnt; }
 cleanup() { stopvms 2>/dev/null; }
@@ -147,10 +148,10 @@ vmrunx - $nfsclnt -- 'command -v authselect && { authselect test -a sssd with-mk
 
 #-------------------------------------------------------------------------------
 #nfs-server: configure krb5 nfs server
-vmrunx - $nfsserv -- make-nfs-server.sh --prefix=$NFSSHARE --nfsroot=/var --no-tlshd
-vmrunx 0 $nfsserv -- "chown :qe $NFSSHARE/qe; chown :devel $NFSSHARE/devel"
-vmrunx 0 $nfsserv -- chmod g+ws $NFSSHARE/qe $NFSSHARE/devel
-vmrunx - $nfsserv -- ls -l $NFSSHARE
+vmrunx - $nfsserv -- make-nfs-server.sh --prefix=$NFSSHARE --nfsroot=$NFSROOT --no-tlshd
+vmrunx 0 $nfsserv -- "chown :qe $NFSROOT/$NFSSHARE/qe; chown :devel $NFSROOT/$NFSSHARE/devel"
+vmrunx 0 $nfsserv -- chmod g+ws $NFSROOT/$NFSSHARE/qe $NFSROOT/$NFSSHARE/devel
+vmrunx - $nfsserv -- ls -l $NFSROOT/$NFSSHARE
 
 vmrunx - $nfsserv -- ipa service-add nfs/${nfsserv}.${domain}
 vmrunx - $nfsserv -- ipa-getkeytab -s ${ipaserv}.${domain} -p nfs/${nfsserv}.${domain} -k /etc/krb5.keytab
