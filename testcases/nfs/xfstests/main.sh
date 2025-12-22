cae46e54f1191e73c0eb290e3b795eb9e071aa71
Jianhong Yin <jiyin@redhat.com>
2025-12-22 11:47:19 +0800

    nfs/xfstests/main.sh: add extra partition for xfstests test
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- testcases/nfs/xfstests/main.sh
+++ testcases/nfs/xfstests/main.sh
@@ -30,7 +30,7 @@ stdlog=$(trun vm create $distro --downloadonly "$@" |& tee /dev/tty)
 imgf=$(sed -rn '${/^-[-rwx]{9}.? /{s/^.* //;p}}' <<<"$stdlog")
 [[ -n "${imgf}" ]] && insOpt=-I=$imgf
 
-trun -tmux vm create $distro -n $nfsserv -m 4G -f -nointeract -p ${pkglist}     "$@" $insOpt
+trun -tmux vm create $distro -n $nfsserv -m 4G -f -nointeract -p ${pkglist} --xdisk 10,xfs --xdisk 10,xfs "$@" $insOpt
 trun       vm create $distro -n $nfsclnt -m 6G -f -nointeract -p ${pkglist},git "$@" $insOpt || exit $?
 echo "{INFO} waiting all vm create process finished ..."
 while ps axf|grep tmux.new.*$$-$USER.*-d.vm.creat[e]; do sleep 10; done
